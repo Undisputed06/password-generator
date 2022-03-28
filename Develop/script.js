0ef661f6eb58e49af3de0c01ac34ee172e7f4f40
Gary Williams <w_gwilliams@yahoo.com>
2022-03-27 21:17:44 -0500

    added random selector functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,103 @@
 // Assignment code here
 
-var alphUpper = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var alphLower = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var nums = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var special = [ " ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "|", "\\", ":", ";", "\"", "'", "<", ">", "?", ".", "~", "`"]
+var upperArray = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numArray = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialArray = [ " ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "|", "\\", ":", ";", "\"", "'", "<", ">", "?", ".", "~", "`"]
+var selectedChars = []
+
+  
 
 
-
-//Function to GET and Check Length 
-var getLength = function() {
-  var passLength = parseInt(window.prompt("How many characters would you like your password to be? Please select between 8 and 128"))
-  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-    window.alert("Please enter a number between 8 - 128")
-    return passLength
-}
-
+//Get random lower case letter
 var getLower = function () {
-  //Get random lower case letter
-  var confirmLower = window.confirm("Would you like Lower Case Characters in your password?")
-  if(confirmLower) {
-    var  randomLower = alphLower[Math.floor(Math.random() * alphLower.length)];
-   return randomLower
+    var randomLower = lowerArray[Math.floor(Math.random() * lowerArray.length)];
+    return randomLower
 }
 
+ //Get random upper case letter 
 var getUpper = function() {
-  //Get random upper case letter 
-  var confirmUpper = window.confirm("Would you like Upper Case Characters in your password")
-  if(confirmUpper){
-   var  randomUpper = alphUpper[Math.floor(Math.random() * alphUpper.length)];
+   var  randomUpper = upperArray[Math.floor(Math.random() * upperArray.length)];
    return randomUpper
 }
 
+ //Get random number 
 var getNum = function() {
-  //Get random number 
-  var confirmNum = window.confirm("Would you like numbers in your password?")
-      if (confirmNum) {
-        var  randomNum = nums[Math.floor(Math.random() * nums.length)];
-        return randomNum
+    var  randomNum = numArray[Math.floor(Math.random() * numArray.length)];
+    return randomNum
 }
 
+//Get random special character 
 var getSpecial = function () {
-  //Get random special character 
-  var confirmSpecial = window.confirm("Would you Special Characters in your password")
-  if(confirmSpecial){
-   var  randomSpecial = special[Math.floor(Math.random() * special.length)];
-   return randomUpper
+   var  randomSpecial = specialArray[Math.floor(Math.random() * specialArray.length)];
+   return randomSpecial
 }
 
+///Get random character from built selectedChars Array 
+var getRandomSelectedChars = function () {
+  var randomChar = selectedChars[Math.floor(Math.random() * selectedChars.length)];
+  return randomChar
+}
 
+var shuffleString = function() {
+  var
+}
 
 var generatePassword = function (){
-      
-      }
-    
+  //Ask User for password criteria 
+  var passLength = parseInt(window.prompt("How many characters would you like your password to be? Please select between 8 and 128"))
+  if (passLength < 8 || passLength >128){
+    window.alert("The password length must be between 8 and 128 characters!")
+    generatePassword()
+  }
+  //Ensure we get a number back
+  if (isNaN(passLength)){
+    window.alert("Please enter a numerical value!")
+    generatePassword()
+  }
+
+  var confirmLower = window.confirm("Would you like Lower Case Characters in your password?");
+  var confirmUpper = window.confirm("Would you like Upper Case Characters in your password");
+  var confirmNum = window.confirm("Would you like numbers in your password?")
+  var confirmSpecial = window.confirm("Would you Special Characters in your password") 
+
+  //Append values to string to ensure the user selection is included atleast once is password
+  var userPass = ''
+  if (confirmLower) {
+    selectedChars = selectedChars.concat(lowerArray)
+    userPass += getLower()
+  }
+
+  if (confirmUpper){
+    selectedChars = selectedChars.concat(upperArray)
+    userPass += getUpper()
+  }
+
+  if (confirmNum){
+    selectedChars = selectedChars.concat(numArray)
+    userPass += getNum()
+  }
+
+  if (confirmSpecial) {
+    selectedChars = selectedChars.concat(specialArray)
+    userPass += getSpecial()
+  }
+
+  var selectedPassword = "";
+
+/*Iterate through selectedChars only grabbing enough characters
+  to build to the password string created by users choices(userPass). 
+*/
+  for (var i =0; (i < passLength - userPass.length); i ++){
+    var char = getRandomSelectedChars()
+    selectedPassword += char
+  }
+ var newGeneratedPassword = selectedPassword + userPass
+ console.log(newGeneratedPassword)
+
+  
+}
+
      
     
 
@@ -66,7 +111,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
